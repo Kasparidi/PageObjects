9e629bda1b91d95335fe3ed8d1da68bd036adc1b
Kasparidi <kasparidi@gmail.com>
2020-12-12 15:21:30 +0300

    initial commit

---
+++ src/test/java/ru/netology/test/MoneyTransferTest.java
@@ -0,0 +1,21 @@
+package ru.netology.test;
+
+import lombok.val;
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
+import ru.netology.page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class MoneyTransferTest {
+    @Test
+    void shouldTransferMoneyBetweenOwnCards() {
+        open("http://localhost:9999");
+        val loginPage = new LoginPage();
+//    val loginPage = open("http://localhost:9999", LoginPageV1.class);
+        val authInfo = DataHelper.getAuthInfo();
+        val verificationPage = loginPage.validLogin(authInfo);
+        val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        verificationPage.validVerify(verificationCode);
+    }
+}
